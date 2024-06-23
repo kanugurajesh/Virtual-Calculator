0901a1ce96d629e92b9be8e32cc4196633c8681c
kanugurajesh <kanugurajesh3@gmail.com>
2024-06-23 20:19:58 +0530

    modified the code

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Keyboard from "./components/Keyboard";
 
 function App() {
   return (
-    <div className="p-10 w-screen h-screen flex flex-col gap-20 ">
-      <textarea name="" id="" className="w-full h-28 border-2 border-black rounded-md p-2" placeholder="Enter the text here"></textarea>
+    <div className="p-10 w-screen h-screen flex flex-col items-center justify-center gap-20">
+      <textarea name="" id="" className="w-full h-48 border-2 border-black rounded-md p-2 font-bold text-lg" placeholder="Enter the text here"></textarea>
       <Keyboard />
     </div>
   );
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,7 +1,35 @@
 const Keyboard = () => {
     return (
-        <div className="bg-black w-full h-full rounded-md ">
-            
+        <div className="w-full h-full border-2">
+            <div className="flex justify-center">
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-gray-800 border-r-white">C</button>
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-gray-800 border-r-white">+/-</button>
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-gray-800 border-r-white">%</button>
+                <button className="bg-orange-500 text-white text-2xl w-1/4 h-20 border-2 border-gray-800 border-r-white">/</button>
+            </div>
+            <div className="flex justify-center">
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">7</button>
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">8</button>
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">9</button>
+                <button className="bg-orange-500 text-white text-2xl w-1/4 h-20 border-2 border-white">X</button>
+            </div>
+            <div className="flex justify-center">
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">4</button>
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">5</button>
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">6</button>
+                <button className="bg-orange-500 text-white text-2xl w-1/4 h-20 border-2 border-white">-</button>
+            </div>
+            <div className="flex justify-center">
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">1</button>
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">2</button>
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">3</button>
+                <button className="bg-orange-500 text-white text-2xl w-1/4 h-20 border-2 border-white">+</button>
+            </div>
+            <div className="flex justify-center">
+                <button className="bg-gray-800 text-white text-2xl w-1/2 h-20 border-2 border-white">0</button>
+                <button className="bg-gray-800 text-white text-2xl w-1/4 h-20 border-2 border-white">.</button>
+                <button className="bg-orange-500 text-white text-2xl w-1/4 h-20 border-2 border-white">=</button>
+            </div>                
         </div>
     )
 }
