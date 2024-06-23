6a07cdbae42346897b97501bd21e9008e6756ec7
kanugurajesh <kanugurajesh3@gmail.com>
2024-06-23 21:33:11 +0530

    modified the code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Keyboard from "./components/Keyboard";
 function App() {
   return (
     <div className="p-10 w-screen h-screen flex flex-col items-center justify-center gap-16">
-      <textarea name="" id="" className="w-full h-52 border-2 border-black rounded-md p-2 font-bold text-lg" placeholder="Enter the text here"></textarea>
+      <textarea name="" id="" className="w-full min-h-[120px] border-2 border-black rounded-md p-2 font-bold text-lg" placeholder="Enter the text here"></textarea>
       <Keyboard />
     </div>
   );
