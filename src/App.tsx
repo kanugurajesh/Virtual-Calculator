4892d8657ba240a690bb56250c92374578490e8d
kanugurajesh <kanugurajesh3@gmail.com>
2024-06-23 21:52:03 +0530

    Changed height

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,17 @@ import Keyboard from "./components/Keyboard";
 
 function App() {
   return (
-    <div className="p-5 w-screen h-screen flex flex-col justify-between overflow-hidden">
-      <textarea
-        name=""
-        id=""
-        className="w-full h-1/2 border-2 border-black rounded-md p-2 font-bold text-lg mb-12"
-        placeholder="Enter the text here"
-      ></textarea>
-      <div className="flex-grow"></div>
-      <Keyboard />
+    <div className="p-5 w-screen h-screen">
+      <div className="flex flex-col items-center justify-between">
+        <textarea
+          name=""
+          id=""
+          className="w-full h-1/2 border-2 border-black rounded-md p-2 font-bold text-lg mb-12"
+          placeholder="Enter the text here"
+        ></textarea>
+        <div className="flex-grow"></div>
+        <Keyboard />
+      </div>
     </div>
   );
 }
