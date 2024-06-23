2bedaf2e920068d046b59f4c9a337238c508cc1a
kanugurajesh <kanugurajesh3@gmail.com>
2024-06-23 22:11:01 +0530

    Changed height

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,3 @@
-// import Keyboard from "./components/Keyboard";
-
-// function App() {
-//   return (
-//     <div className="p-5 w-screen h-screen flex flex-col items-center justify-between gap-16 overflow-hidden">
-//       <textarea name="" id="" className="w-full min-h-[150px] border-2 border-black rounded-md p-2 font-bold text-lg" placeholder="Enter the text here"></textarea>
-//       <Keyboard />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import Keyboard from "./components/Keyboard";
 
 function App() {
@@ -20,7 +7,7 @@ function App() {
         <textarea
           name=""
           id=""
-          className="w-full h-1/2 border-2 border-black rounded-md p-2 font-bold text-lg mb-12"
+          className="w-full h-1/3 border-2 border-black rounded-md p-2 font-bold text-lg mb-12"
           placeholder="Enter the text here"
         ></textarea>
         <div className="flex-grow"></div>
